c3ee04aaecd29684dac300b9eebbcfdb86036e40
victor.almeida.fonseca <victor.fonseca@toroinvestimentos.com.br>
2022-01-24 15:58:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+const wb = require('./ws')
+ 
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`App Express is running!`);
+})
+
+const wss = wb(server)
+
+setInterval(() => {
+    wss.broadcast({n : Math.random() }, 1000)
+})
